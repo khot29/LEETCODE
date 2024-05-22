3bd49bed2b711b9847b67c8a61e140374675b958
Huzaif_Khot <mailtohuzaifkhot@gmail.com>
2024-05-22 21:18:55 +0530

    More js Problems

---
+++ 30 DAYS OF JAVASCRIPT/EASY/2626. Array Reduce Transformation.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+var reduce = function (nums, fn, init) {
+  let accum = 0;
+
+  if (!nums.length) {
+    return init;
+  }
+
+  accum += init;
+  for (let i = 0; i < nums.length; i++) {
+    accum = fn(accum, nums[i]);
+  }
+
+  return accum;
+};
